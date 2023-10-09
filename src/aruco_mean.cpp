41e83af85623fc84f230919b5e6b8e7fa7b1bd27
tiago369 <tiagobarreto581@gmail.com>
2023-10-08 23:13:59 -0300

    Add offset to node

--- src/aruco_mean.cpp
+++ src/aruco_mean.cpp
@@ -13,37 +13,69 @@ class ArucoMean : public rclcpp::Node
     ArucoMean()
     : Node("aruco_mean")
     {
+        std::vector<long int> ids = {0,1,2,3};
+        std::vector<double> left_offset = {0.0,0.0,0.0};
+        std::vector<double> right_offset = {0.0,0.0,0.0};
+
         this->declare_parameter("yaml_name", "yaml.yaml");
+        this->declare_parameter("left_offset", left_offset);
+        this->declare_parameter("right_offset", right_offset);
+        this->declare_parameter("aruco_ids", ids);
 
         yaml_name_ = this->get_parameter("yaml_name").as_string();
+        left_offset_ = this->get_parameter("left_offset").as_double_array();
+        right_offset_ = this->get_parameter("right_offset").as_double_array();
+        ids_ = this->get_parameter("aruco_ids").as_integer_array();
+
         left_subscription_ = this->create_subscription<ros2_aruco_interfaces::msg::ArucoMarkers>(
             "left_topic", 10,std::bind(&ArucoMean::left_aruco_subscriber,
             this, std::placeholders::_1));
         right_subscription_ = this->create_subscription<ros2_aruco_interfaces::msg::ArucoMarkers>(
             "right_topic", 10,std::bind(&ArucoMean::right_aruco_subscriber,
             this, std::placeholders::_1));
-        pose_publisher_ = this->create_publisher<geometry_msgs::msg::Pose>("pose", 10);
+        pose_publisher_ = this->create_publisher<ros2_aruco_interfaces::msg::ArucoMarkers>("pose", 10);
     }
 
     private:
 
+    bool areVectorsEqual(const std::vector<long int> vec1, const std::vector<long int> vec2) {
+        // Check if the sizes are different
+        if (vec1.size() != vec2.size()) {
+            return false;
+        }
+
+        // Sort them
+        std::vector<long int> sortedVec1 = vec1;
+        std::vector<long int> sortedVec2 = vec2;
+
+        std::sort(sortedVec1.begin(), sortedVec1.end());
+        std::sort(sortedVec2.begin(), sortedVec2.end());
+
+        // Compare the sorted vectors element-wise
+        return sortedVec1 == sortedVec2;
+    }
+
     void left_aruco_subscriber(const ros2_aruco_interfaces::msg::ArucoMarkers & msg) {
         std::lock_guard<std::mutex> lock(mutex_);
-        if (static_cast<int>(left_aruco_.size()) <= buffer_size_) {
-            left_aruco_.push_back(msg);
-        } else {
-            left_aruco_.erase(left_aruco_.begin());
-            left_aruco_.push_back(msg);
+        if(areVectorsEqual(ids_, msg.marker_ids)){
+            if (static_cast<int>(left_aruco_.size()) <= buffer_size_) {
+                left_aruco_.push_back(msg);
+            } else {
+                left_aruco_.erase(left_aruco_.begin());
+                left_aruco_.push_back(msg);
+            }
         }
     }
 
     void right_aruco_subscriber(const ros2_aruco_interfaces::msg::ArucoMarkers & msg) {
         std::lock_guard<std::mutex> lock(mutex_);
-        if (static_cast<int>(right_aruco_.size()) <= buffer_size_) {
-            right_aruco_.push_back(msg);
-        } else {
-            right_aruco_.erase(right_aruco_.begin());
-            right_aruco_.push_back(msg);
+        if(areVectorsEqual(ids_, msg.marker_ids)){
+            if (static_cast<int>(right_aruco_.size()) <= buffer_size_) {
+                right_aruco_.push_back(msg);
+            } else {
+                right_aruco_.erase(right_aruco_.begin());
+                right_aruco_.push_back(msg);
+            }
         }
     }
 
@@ -65,14 +97,26 @@ class ArucoMean : public rclcpp::Node
         return result;
     }
 
-    geometry_msgs::msg::Pose aruco_mean(ros2_aruco_interfaces::msg::ArucoMarkers one,
+    ros2_aruco_interfaces::msg::ArucoMarkers aruco_mean(ros2_aruco_interfaces::msg::ArucoMarkers one,
                                   ros2_aruco_interfaces::msg::ArucoMarkers two) {
-        geometry_msgs::msg::Pose result = geometry_msgs::msg::Pose();
-        for(int i=0; i < one.marker_ids.size(); i++){
-            result.position.x = (one.poses.at(i).position.x + two.poses.at(i).position.x)/2;
-            result.position.y = (one.poses.at(i).position.y + two.poses.at(i).position.y)/2;
-            result.position.z = (one.poses.at(i).position.z + two.poses.at(i).position.z)/2;
+        ros2_aruco_interfaces::msg::ArucoMarkers result = ros2_aruco_interfaces::msg::ArucoMarkers();
+        for(int i=0; i < static_cast<int>(one.marker_ids.size()); i++){
+            result.poses.at(i).position.x = (one.poses.at(i).position.x + two.poses.at(i).position.x)/2;
+            result.poses.at(i).position.y = (one.poses.at(i).position.y + two.poses.at(i).position.y)/2;
+            result.poses.at(i).position.z = (one.poses.at(i).position.z + two.poses.at(i).position.z)/2;
         }
+        return result;
+    }
+
+    ros2_aruco_interfaces::msg::ArucoMarkers add_offset(ros2_aruco_interfaces::msg::ArucoMarkers aruco, std::vector<double> offset){
+        ros2_aruco_interfaces::msg::ArucoMarkers result = ros2_aruco_interfaces::msg::ArucoMarkers();
+        // result.marker_ids = aruco.at(0).marker_ids;
+        for(int j=0; j < static_cast<int>(aruco.marker_ids.size()); j++){
+            result.poses.at(j).position.x += offset.at(0);
+            result.poses.at(j).position.y += offset.at(1);
+            result.poses.at(j).position.z += offset.at(2);
+            }
+
         return result;
     }
 
@@ -94,22 +138,26 @@ class ArucoMean : public rclcpp::Node
             return;
         }
         // Calculate the mean off the points
-        geometry_msgs::msg::Pose pose;
-        pose = aruco_mean(vector_mean(left_aruco_),
-                          vector_mean(right_aruco_));
+        ros2_aruco_interfaces::msg::ArucoMarkers pose;
+        pose = aruco_mean(add_offset(vector_mean(left_aruco_), left_offset_),
+                          add_offset(vector_mean(right_aruco_), right_offset_));
         mutex_.unlock();
         pose_publisher_->publish(pose);
-        
+
+        return;
     }
 
     rclcpp::Subscription<ros2_aruco_interfaces::msg::ArucoMarkers>::SharedPtr left_subscription_;
     rclcpp::Subscription<ros2_aruco_interfaces::msg::ArucoMarkers>::SharedPtr right_subscription_;
-    rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr pose_publisher_;
+    rclcpp::Publisher<ros2_aruco_interfaces::msg::ArucoMarkers>::SharedPtr pose_publisher_;
     std::vector<ros2_aruco_interfaces::msg::ArucoMarkers> left_aruco_{};
     std::vector<ros2_aruco_interfaces::msg::ArucoMarkers> right_aruco_{};
     int buffer_size_{50};
     std::string yaml_name_;
     std::mutex mutex_;
+    std::vector<long int> ids_;
+    std::vector<double> left_offset_;
+    std::vector<double> right_offset_;
 };
 
 int main(int argc, char ** argv)
